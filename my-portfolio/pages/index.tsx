c89543972981a7686bbef09e748266d1b4ef49a2
Neelima <neelimaakkiraju2001@gmail.com>
2024-06-05 11:26:28 +0530

    new changes

---
+++ my-portfolio/pages/index.tsx
@@ -0,0 +1,10 @@
+import Layout from '../components/Layout';
+
+export default function Home() {
+  return (
+    <Layout>
+      <h2 className="text-3xl font-bold">Welcome to My Portfolio</h2>
+      <p className="mt-4">This is the homepage of my portfolio. Here you can find information about me and my work.</p>
+    </Layout>
+  );
+}
---
+++ my-portfolio/pages/projects.tsx
@@ -0,0 +1,10 @@
+import Layout from '../components/Layout';
+
+export default function Projects() {
+  return (
+    <Layout>
+      <h2 className="text-3xl font-bold">My Projects</h2>
+      <p className="mt-4">This is the projects page. Here you can find a list of my projects.</p>
+    </Layout>
+  );
+}
